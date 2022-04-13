43320e85f07314df2b798abc32786a0637419d14
Anais Casanova <anais.linda.v@gmail.com>
2022-04-14 01:11:27 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,57 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    dots:false,
     nav:true,
-    autoplay:true,   
-    smartSpeed: 3000, 
-    autoplayTimeout:7000,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:2
+            items:3
         },
         1000:{
-            items:3
+            items:5
         }
     }
 })
+
+// fetch('https://api.unsplash.com/photos/random/?count=5&client_id=52d8369eb3e2576a5f5b6423865e074e9c7045761bff1ac5664ff3e0bdb57a1d') 
+//   .then(response => response.json())
+//   .then(data => {
+//     data.forEaach(function(image, i) {
+//       document.querySelector("#slide-" + (i+1)).innerHTML = `
+//         <img src="${image.urls.regular}" alt="">
+//         <p class="author-info">
+//           <a href="${image.links.html}?utm_source=slider-thing&utm_medium=referral&utm_campaign=api-credit">Photo by ${image.user.name}</a> on <a href="https://unsplash.com/">Unsplash</a>
+//         </p>
+//       `;
+//     });
+//   });
